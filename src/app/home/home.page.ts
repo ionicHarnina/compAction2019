0856b18a6effdbbc617d18aba579ea54f05b1ec7
programacion <>
2019-11-13 10:40:50 +0100

    incial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetOptions } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  mostrar = false;
+  // Inyectando la dependencia del sheet controller
+  opciones: ActionSheetOptions;
+  constructor(public actionSheetController: ActionSheetController) {
+    this.opciones = { header: 'hola', buttons: [] };
+  }
 
-  constructor() {}
-
+  // Aqui definimos el action sheet, con sus botones, etc
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albuns',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            console.log('Delete clicked');
+            this.mostrar = true;
+          }
+        }, {
+          text: 'Share',
+          icon: 'share',
+          handler: () => {
+            console.log('Share clicked');
+          }
+        }, {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }, {
+          text: 'Play (open modal)',
+          icon: 'arrow-dropright-circle',
+          handler: () => {
+            console.log('Play clicked');
+          }
+        }, {
+          text: 'Favorite',
+          icon: 'heart',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }
+      ]
+    });
+    await actionSheet.present();
+  }
 }
